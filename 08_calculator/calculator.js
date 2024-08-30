8ad3b9ea322ab014236169a37ef8820e9403a371
grateqq <156215485+grateqq@users.noreply.github.com>
2024-08-30 11:02:49 -0300

    Update calculator.js
    funcinando

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,28 +7,7 @@ const subtract = function(num1, num2) {
 	return num1- num2
 };
 
-const sum = function(array) {
-	return array.reduce((contador,value)=> contador += value,0)
-};
 
-const multiply = function(num1,num2) {
-  return num1 *num2
-
-};
-
-const power = function(num1, num2) {
-	return num1 ^ num2
-};
-
-const factorial = function(num1) {
-  let contador = num1
-  for (i=num1; i > 1; i--) {
-    console.log(i) // 5
-    contador *= i
-    }
-
-	return contador
-};
 
 // Do not edit below this line
 module.exports = {
@@ -38,4 +17,32 @@ module.exports = {
   multiply,
   power,
   factorial
+};
+const sum = function(array) {
+	return array.reduce((contador,value)=> contador += value,0)
+};
+
+const multiply = function(array) {
+  return array.reduce((result,num,)=>result *= num, 1)
+
+
+};
+
+const power = function(num1, num2) {
+	return num1 ** num2
+};
+
+const factorial = function(num1) {
+  let contador = 1
+  if (num1 === 0) {
+    contador = 1
+  } else {
+    for (i=num1; i >= 1; i--) {
+    console.log("Vuetla : " + i) // 5
+    contador *= i
+    console.log(contador)
+    }}
+  
+	return contador
+
 };
