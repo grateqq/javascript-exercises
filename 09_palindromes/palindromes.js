5d5cce6a1c86859f006425e143d24ec2b85dce96
granaqq <156215485+grateqq@users.noreply.github.com>
2024-09-02 02:23:27 -0300

    lo viejo

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
-"holaS"
+const palindromes = function (word) {
+  word = word.split("") 
+  console.log(word.length/2) 
+  console.log(word.length)
+  console.log(word)
+  wordlong = Math.floor(word.length / 2)
+  console.log(wordlong)
+  
+  console.log("inicio")
+  
+  for (i=0; i<wordlong; i ++) {
+    //console.log(word[i])
+    //console.log(word[word.length-1-i])
+    let a = word[i]
+    let b = word[word.length-1-i]
+    console.log(a)
+    console.log(b)
+    if (a !== b) {return false}
+    }
+  
+    return true
 };
 
 // Do not edit below this line
