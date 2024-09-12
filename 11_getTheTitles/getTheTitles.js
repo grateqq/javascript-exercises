4e710794d4a113c846eb75ecc581b6c62f5e00d2
grateqq <156215485+grateqq@users.noreply.github.com>
2024-09-12 12:59:08 -0300

    Update getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+  let arrayTitles = []
+  array.forEach(element => {arrayTitles.push(element.title)
+    
+  });
 
+  console.log(arrayTitles)
+  return arrayTitles
 };
 
 // Do not edit below this line
