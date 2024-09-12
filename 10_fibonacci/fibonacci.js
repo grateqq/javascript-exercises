9ae5ee579d63819ce57ed2b5d49fa16788480d93
grateqq <156215485+grateqq@users.noreply.github.com>
2024-09-12 12:58:40 -0300

    Update fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(num) {
+  let numbers = [0,1,1];
+  // num 4
+  num = +num
+  if (num==0) {return 0}; 
+  if (num==1) {return 1}; 
+  if (num < 0) {return "OOPS"}
+  num = +num
+  for (i=2; i<num; i ++) {
+      //console.log("ciclo: " + i)
+      //console.log(numbers[i]);
+      //console.log(numbers[i-1]);
+      numbers.push(numbers[i] + numbers[i-1]);
+      //console.log(numbers)
+    }
+  
+    //console.log("--------- FIN")
+  return numbers[num-1] + numbers[num-2];
+  }
 
 // Do not edit below this line
 module.exports = fibonacci;
