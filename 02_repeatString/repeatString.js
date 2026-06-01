73163b753d126805b5f8bf4d5bf2945e6863301c
grateqq <156215485+grateqq@users.noreply.github.com.>
2026-06-01 19:23:20 -0300

    repeatString Pass test 1 "repeats the string "

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (word, num) {
+  var saveword = word;
+  for (i = 1; i < num; i++) {
+    saveword = saveword.concat(word);
+  }
+  return saveword;
 };
 
 // Do not edit below this line
