94b6fa576b91719e6d5c8dce0f0a36e402d72124
grateqq <156215485+grateqq@users.noreply.github.com.>
2026-06-02 01:54:08 -0300

    reserse complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  var reverse = "";
+  for (var i = 0; i < word.length; i++) {
+    reverse = reverse.concat(word[word.length - 1 - i]);
+  }
+  return reverse;
 };
 
 // Do not edit below this line
