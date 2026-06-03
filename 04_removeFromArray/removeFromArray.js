13234060854f4f96bf9c5c27c25a4aae35a3f064
grateqq <156215485+grateqq@users.noreply.github.com.>
2026-06-03 19:14:02 -0300

    pass 1 ,2 y 3 removefrom

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function (array, a) {
-  const find = array.findIndex((item) => a === item);
-  array.splice(find, 1);
+const removeFromArray = function (array, ...itemdelete) {
+  for (let i = 0; i < itemdelete.length; i++) {
+    while (array.findIndex((item) => itemdelete[i] === item) !== -1) {
+      const find = array.findIndex((item) => itemdelete[i] === item);
+      array.splice(find, 1);
+    }
+  }
   return array;
 };
 
