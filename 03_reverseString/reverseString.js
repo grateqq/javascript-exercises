942472af12b833b0e7cf17d476017876b5ea5b42
grateqq <156215485+grateqq@users.noreply.github.com.>
2026-06-03 10:37:20 -0300

    pass el 1er test removefron array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,20 @@
 const reverseString = function (word) {
-  var reverse = "";
-  for (var i = 0; i < word.length; i++) {
-    reverse = reverse.concat(word[word.length - 1 - i]);
+  let reverse = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    reverse += word[i];
   }
   return reverse;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+//refactorizado
+// const reverseString = function (word) {
+//   var reverse = "";
+//   for (var i = 0; i < word.length; i++) {
+//     reverse = reverse.concat(word[word.length - 1 - i]);
+//   }
+//   return reverse;
+
+// };
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, a) {
+  const find = array.findIndex((item) => a === item);
+  array.splice(find, 1);
+  return array;
 };
 
 // Do not edit below this line
