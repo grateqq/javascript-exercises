a24b7148e04261bf6b47ce8c37e1f378574912e4
grateqq <156215485+grateqq@users.noreply.github.com.>
2026-06-06 00:46:52 -0300

    completado el 8 temp conv

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempF) {
+  // De Fahrenheit a Celsius
+  const tempC = (tempF - 32) * (5 / 9);
+  let rounded = Math.round(tempC * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempC) {
+  //De Celsius a Fahrenheit
+  const tempF = tempC * (9 / 5) + 32;
+  let rounded = Math.round(tempF * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
