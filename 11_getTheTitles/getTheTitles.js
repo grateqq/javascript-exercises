716aa6e9a1bfc5201e65227bc9b1976d229e2d19
grateqq <156215485+grateqq@users.noreply.github.com.>
2026-06-07 12:30:49 -0300

    Completado 11 GetTitles con forEach

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,10 @@
-const getTheTitles = function (array) {};
+const getTheTitles = function (array) {
+  const resultado = [];
+  array.forEach((element) => {
+    resultado.push(element.title);
+  });
+  return resultado;
+};
 
 // Do not edit below this line
 module.exports = getTheTitles;
