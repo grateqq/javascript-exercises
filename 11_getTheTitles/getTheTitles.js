6ec3339b2ee801c0f24485f6c9c190f5c1a5dcbf
grateqq <156215485+grateqq@users.noreply.github.com.>
2026-06-07 12:36:47 -0300

    completado 11 refactorizacion con map

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,10 +1,16 @@
 const getTheTitles = function (array) {
-  const resultado = [];
-  array.forEach((element) => {
-    resultado.push(element.title);
-  });
-  return resultado;
+  return array.map((obj) => obj.title);
 };
+
+// Con forEach
+
+// const getTheTitles = function (array) {
+//   const resultado = [];
+//   array.forEach((element) => {
+//     resultado.push(element.title);
+//   });
+//   return resultado;
+// };
 
 // Do not edit below this line
 module.exports = getTheTitles;
